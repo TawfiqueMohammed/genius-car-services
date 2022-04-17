0af24f2c5b1791d43619bcd7b050fe3042266a7e
Tawfique Mohammed <tawfiquemt17@gmail.com>
2022-04-18 00:54:43 +0600

    module 62 completed

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,44 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import './Register.css';
 import auth from '../../../firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
+import { sendEmailVerification } from 'firebase/auth';
+import Loading from '../../Shared/Loading/Loading';
 
 const Register = () => {
+
+    const [agree, setAgree] = useState(false);
     const [
         createUserWithEmailAndPassword,
         user,
         loading,
         error,
-    ] = useCreateUserWithEmailAndPassword(auth);
+    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+
     const navigate = useNavigate();
     const navigateLogin = () => {
         navigate('/login')
     }
 
-    if (user) {
-        navigate('/login');
+    if (loading || updating) {
+        return <Loading></Loading>
     }
 
-    const handleRegister = event => {
+    if (user) {
+        console.log('user: ', user);
+    }
+
+    const handleRegister = async (event) => {
         event.preventDefault();
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
-
-        createUserWithEmailAndPassword(email, password);
+        // const agree = event.target.terms.checked;  ...ebhabe o kora jaay uporer useState use na kore...
+        await createUserWithEmailAndPassword(email, password);
+        await updateProfile({ displayName: name });
+        console.log('Updated profile');
+        navigate('/home')
     }
+
+
+
 
     return (
         <div className='register-form'>
-            <h2 style={{ textAlign: 'center' }}>Please Register</h2>
+            <h2 className='text-primary' style={{ textAlign: 'center' }}>Please Register</h2>
             <form onSubmit={handleRegister}>
                 <input type="text" name='name' id='' placeholder='Your Name' />
-                <input type="email" name='email' id='' placeholder='Email Address' />
-                <input type="password" name='password' id='' placeholder='Password' />
-                <input className='btn btn-primary' type="submit" value="register" />
-                <p>Already Registered? <Link to='/login' className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
+                <input type="email" name='email' id='' placeholder='Email Address' required />
+                <input type="password" name='password' id='' placeholder='Password' required />
+
+                <input onClick={() => setAgree(!agree)} type="checkbox" name="terms" id="terms" />
+                {/* <label className={agree ? 'ps-2 text-primary' : 'ps-2 text-danger'} htmlFor="terms">Accept Genius Car terms and condtions</label> */}
+                <label className={`ps-2 ${agree ? '' : 'text-danger'}`} htmlFor="terms">Accept Genius Car terms and condtions</label>
+
+                <input
+                    disabled={!agree}
+                    className='btn btn-primary w-50 mx-auto d-block mb-2 mt-2'
+                    type="submit"
+                    value="Register" />
             </form>
+            <p>Already have an account? <Link to='/login' className='text-primary pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
+            <SocialLogin></SocialLogin>
         </div>
     );
 };
